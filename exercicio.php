5d42f6668437fcfe51268b7d601ebbf95a7d6e02
Lincoln Berto dos Santos <lincoln.santospro@gmail.com>
2023-12-02 22:43:16 -0300

    finalização do projeto para visualização dos exercicios

--- exercicio.php
+++ exercicio.php
@@ -2,31 +2,30 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200;300;400;500;600;700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="assets\css\estilo.css">
-    <link rel="stylesheet" href="assets\css\exercicio.css">
+    <link href="https://fonts.googleapis.com/css?family=Oswald:200,300,400,500,600,700" rel="stylesheet">
+    <link rel="stylesheet" href="recursos/css/estilo.css">
+    <link rel="stylesheet" href="recursos/css/exercicio.css">
     <title>Exercício</title>
 </head>
 <body class="exercicio">
-
     <header class="cabecalho">
         <h1>Curso PHP</h1>
         <h2>Visualização do Exercício</h2>
     </header>
     <nav class="navegacao">
-        <a href="#" class="verde">Sem formatação</a>
+        <a href=<?= "/{$_GET['dir']}/{$_GET['file']}.php" ?> 
+            class="verde">Sem formatação</a>
         <a href="index.php" class="vermelho">Voltar</a>
     </nav>
     <main class="principal">
         <div class="conteudo">
-            
+            <?php
+                include(__DIR__ . "/{$_GET['dir']}/{$_GET['file']}.php");
+            ?>
         </div>
     </main>
     <footer class="rodape">
-        COD3R & ALUNOS © <?= date('Y')?>
+        COD3R & ALUNOS © <?= date('Y'); ?>
     </footer>
-
-</body> 
+</body>
 </html>
