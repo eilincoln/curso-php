e70539d13a797c678f699a3dfab0e7377e7f7700
Lincoln Berto dos Santos <lincoln.santospro@gmail.com>
2023-12-04 19:20:56 -0300

    adicionando noo exercicio e alterando o index.php

---
+++ basico/ola.php
@@ -0,0 +1,17 @@
+<div class="titulo">Olá PHP</div>
+
+<h2>Exemplo A</h2>
+<?php
+    // Lembrando que aqui estou no constexto de uma página HTML.
+    echo "Olá <br>";
+    echo "Mundo!"
+?>
+
+<h2>Exemplo B</h2>
+<?= 
+// posso escrever assim, mas nesse caso é para imprimir direto algo na tela. 
+"posso me expressar assim" 
+?>
+
+<?php
+phpinfo();
