73e57fac47b03eb15face9efcfca7be0ee1ff3d3
Lincoln Berto dos Santos <lincoln.santospro@gmail.com>
2023-12-05 10:21:11 -0300

    add anotações sobre ponto flutuante + alteração no HTML

--- index.php
+++ index.php
@@ -49,6 +49,9 @@
                         <li>
                             <a href="exercicio.php?dir=tipos&file=int">Tipo Inteiro</a>
                         </li>
+                        <li>
+                            <a href="exercicio.php?dir=tipos&file=float">Tipo Float</a>
+                        </li>
                     </ul>
                 </div>
             </nav>
